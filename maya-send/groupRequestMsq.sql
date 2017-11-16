51bcacd9d27b419f0a8f3fa93854b5a2ad56fa10
DESKTOP-14NC32I\Maya Lemmon-Kishi <mel118@pitt.edu>
2017-11-15 22:45:27 -0500

    New trigger??

--- maya-send/groupRequestMsq.sql
+++ maya-send/groupRequestMsq.sql
@@ -1,21 +1,3 @@
-create or replace trigger friendReqMsg
-	after insert on pendingFriends
-	for each row
-	DECLARE
-		msgID varchar(20);
-    begin
-    IF :new.message IS NOT NULL
-    THEN 
-		SELECT to_char(count(msgID) + 1) INTO msgID
-		FROM messages
-		GROUP BY message;
-		
-		insert into message values(msgID,:new.fromID, :new.message, null, :new.toID, current_date);
-		insert into messageRecipient values(:new.msgID, :new.toID);
-    END IF;
-	end;
-/
-
 create or replace trigger groupReqMsg
 	after insert on pendingGroupmembers
 	for each row
