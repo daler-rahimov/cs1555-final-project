b207ff10e97c961edca39174e48827a9ddea5e15
DESKTOP-14NC32I\Maya Lemmon-Kishi <mel118@pitt.edu>
2017-11-15 22:46:27 -0500

    typo

--- triggers/FriendRequestMsgTrig.sql
+++ triggers/FriendRequestMsgTrig.sql
@@ -15,22 +15,3 @@ create or replace trigger friendReqMsg
     END IF;
 	end;
 /
-
-create or replace trigger groupReqMsg
-	after insert on pendingGroupmembers
-	for each row
-	DECLARE
-		msgID varchar(20);
-		managerCount number(20);
-    begin
-    IF :new.message IS NOT NULL
-    THEN 
-		SELECT to_char(count(msgID)) INTO msgID
-		FROM messages
-		GROUP BY message;
-		
-		insert into message values(msgID,:new.fromID, :new.message, null, :new.toID, current_date);
-		insert into messageRecipient values(:new.msgID, :new.toID);
-    END IF;
-	end;
-/
