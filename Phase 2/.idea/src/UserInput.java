63598d76c0fd51ee7a75755670fcf5e997395866
Daler <d.rahimov@petrosoftinc.com>
2017-11-26 21:33:44 -0500

    some more empty classes

---
+++ Phase 2/.idea/src/UserInput.java
@@ -0,0 +1,13 @@
+/*
+ * This class will have static methods to get the user input and validate it 
+ * if it fails the validation it should keep propting untill the correct input 
+ * is entered 
+ */
+
+/**
+ *
+ * @author Daler
+ */
+public class UserInput {
+    
+}
