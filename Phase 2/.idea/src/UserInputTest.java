492af74d0aae53c79d2cae7581caae2dcb70d7d9
Daler <d.rahimov@petrosoftinc.com>
2017-11-30 23:19:24 -0500

    Groups done last method not checked

--- Phase 2/.idea/src/UserInputTest.java
+++ Phase 2/.idea/src/UserInputTest.java
@@ -10,10 +10,12 @@
  */
 public class UserInputTest {
     public static void main(String args[]){
-//        String userID = UserInput.getID();
+        String userID = UserInput.getID();
 //        System.out.println("UserInputTest > userID=" + userID);
 //        String msg = UserInput.getMessage();
 //        System.out.println(msg);
-         UserInput.getInt("Enter something > ");
+//         UserInput.getInt("Enter something > ");
+//        System.out.print( UserInput.getLine20("Enter a name >"));
+        
     }
 }
