81494bc25879cda1f48c8e013bac5118481c75e4
Daler <d.rahimov@petrosoftinc.com>
2017-12-01 20:37:15 -0500

    Maintest

---
+++ Phase 2/MainTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Daler
+ */
+public class MainTest {
+    public static void main (String avg[]){
+        Message.isTest = true; 
+        Message.sentMessageToUser("1");
+        
+    }
+}
