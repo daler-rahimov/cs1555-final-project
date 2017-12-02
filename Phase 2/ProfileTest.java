1b618e604b52a2d145331da5759fa03d0c148663
DESKTOP-14NC32I\Maya Lemmon-Kishi <mel118@pitt.edu>
2017-12-01 19:34:58 -0500

    Instructions to run some methods

--- Phase 2/ProfileTest.java
+++ Phase 2/ProfileTest.java
@@ -1,15 +1,15 @@
 public class ProfileTest {
     public static void main(String args[]){
-        //System.out.print(Profile.login("1", "TMA58U6JG")); //returns false
-        //System.out.print(Profile.login("1", "TMA58URO6JG")); //returns true
-        //Profile.logout("2");
-        Profile.dropUser("9");
-        //Profile.createUser();
-       // Profile.threeDegrees("43", "44"); //direct friends
-        //Profile.threeDegrees("43", "47"); //2 hops
-        //Profile.threeDegrees("43", "48"); //3 hops
-        //Profile.threeDegrees("43", "45"); //no path
-        //Profile.searchForUser();
+        System.out.print(Profile.login()); //returns false, "1", "TMA58U6JG"
+        System.out.print(Profile.login()); //returns true, "1", "TMA58URO6JG"
+        Profile.logout("2");
+        Profile.dropUser("10");
+        Profile.createUser();
+        Profile.threeDegrees(); //direct friends, enter 43 and 44
+        Profile.threeDegrees(); //2 hops, enter 43 and 47
+        Profile.threeDegrees(); //3 hops, enter 43 and 48
+        Profile.threeDegrees(); //no path, enter 43 and 45
+        Profile.searchForUser();
 
     }
 }
