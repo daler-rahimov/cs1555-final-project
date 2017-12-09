add2ba0265e69f257dd5c159f2ab5203bddd8678
Daler <d.rahimov@petrosoftinc.com>
2017-12-09 16:26:01 -0500

    inport delete

--- Phase 2/Group.java
+++ Phase 2/Group.java
@@ -1,13 +1,9 @@
 
-import com.sun.javafx.image.impl.IntArgb;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Group {
 
