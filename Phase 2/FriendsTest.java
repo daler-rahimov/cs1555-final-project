d182ca5b6f21e649323969d0959ac94411dd03d1
Daler <d.rahimov@petrosoftinc.com>
2017-12-09 15:50:04 -0500

    utf-8 fixed

--- Phase 2/FriendsTest.java
+++
@@ -1,8 +0,0 @@
-public class FriendsTest {
-    public static void main(String args[]){
-        Friends.confirmFriendship("4");
-        Friends.confirmFriendship("66");
-        System.out.println(Friends.initiateFriendship("25"));
-        Friends.displayFriends("7"); //Has friends, can test both correct user and incorrect user
-    }
-}
