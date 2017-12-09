06e4336d8df83dfb31d21cd5ae483cab0913f9c5
DESKTOP-14NC32I\Maya Lemmon-Kishi <mel118@pitt.edu>
2017-12-09 18:08:56 -0500

    Stupid 4

--- Phase 2/FriendsTest.java
+++ Phase 2/FriendsTest.java
@@ -1,6 +1,6 @@
 public class FriendsTest {
     public static void main(String args[]){
-        Friends.confirmFriendship("5");
+        Friends.confirmFriendship("6");
         Friends.confirmFriendship("66");
         System.out.println(Friends.initiateFriendship("25"));
         Friends.displayFriends("7"); //Has friends, can test both correct user and incorrect user
